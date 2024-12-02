add184c0280067d426e9079b7f4c8c6914803978
ThomasPDye <tompdye@googlemail.com>
2024-12-02 07:48:48 +0000

    prepare by moving existing engine into engine namespace

--- tests/engine/match_check4b.cpp
+++ tests/engine/match_check4b.cpp
@@ -2,8 +2,8 @@
 
 #include <iostream>
 
-using namespace regex;
-using namespace regex::constructors;
+using namespace regex::engine;
+using namespace regex::engine::constructors;
 
 graph target = optional(concat(single_char('h'), single_char('i')));
 
